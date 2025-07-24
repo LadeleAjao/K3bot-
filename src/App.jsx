ce27632dbcf9d839346d08cbda5f351af76f8b0b
Adigun Oreoluwa <adisoreoluwa@gmail.com>
2025-07-23 21:27:18 -0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Component/Navbar";
+import Home from "./Pages/Home"
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full bg-gray-100 p-3'>
+
+    </nav>
+  )
+}
+
+export default Navbar
