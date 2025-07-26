2aebec6c5d4087fe92c9354084b55dbd09a08d5a
Adigun Oreoluwa <adisoreoluwa@gmail.com>
2025-07-26 15:19:56 -0700

    Blog update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from "../Component/Hero"
 import Features from "../Pages/Features"
+import Blog from "../Pages/Blog"
 
 const Home = () => {
   return (
@@ -11,6 +12,9 @@ const Home = () => {
 
       <section className="w-full m-0 p-0">
         <Features />
+      </section>
+      <section>
+        <Blog/>
       </section>
     </main>
   )
