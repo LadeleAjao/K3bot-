6eeb47ae1bfb152cb2da236992bab21736e44c4d
Adigun Oreoluwa <adisoreoluwa@gmail.com>
2025-07-29 11:35:54 -0700

    Trusted Update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,14 +4,16 @@ import Features from "../Pages/Features"
 import Blog from "../Pages/Blog"
 import Niche from './Niche'
 import Testimony from '../Component/Testimony'
-
+import  Trusted from '../Component/Trusted'
 const Home = () => {
   return (
     <main className="m-0 p-0">
       <section className="lg:h-screen w-full overflow-hidden m-0 p-0">
         <Hero />
       </section>
-
+      <div>
+        <Trusted/>
+      </div>
       <section className="w-full m-0 p-0">
         <Features />
       </section>
